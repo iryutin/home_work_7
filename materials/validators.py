54965208ae739c6c416eccdff3ca89a234136850
Ilia Ryutin <iryutin@mail.ru>
2025-07-08 20:33:59 +0300

    Выполнил дз

--- materials/validators.py
+++ materials/validators.py
@@ -1,7 +1,12 @@
 from django.core.exceptions import ValidationError
 
-def youtubefiltr (video):
-    if 'youtube.com' not in video.lower() and 'youtu.be' not in video.lower():
-        raise ValidationError(
-            f"Ссылки на сторонние ресурсы запрещены. Обнаружена ссылка: {video}"
-        )
+class YoutubeFiltrValidator:
+    def __call__(self, video):
+        if 'youtube.com' not in video.lower() and 'youtu.be' not in video.lower():
+            raise ValidationError(
+                f"Ссылки на сторонние ресурсы запрещены. Обнаружена ссылка: {video}"
+            )
+
+    @classmethod
+    def __fields__(cls):
+        return ['video']
