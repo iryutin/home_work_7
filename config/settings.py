9c52d28dfe67b6385c4d9eef405f488c0a9d3739
Ilia Ryutin <iryutin@mail.ru>
2025-08-19 22:37:26 +0300

    через pip пытаемся

--- config/settings.py
+++ config/settings.py
@@ -4,6 +4,7 @@ from datetime import timedelta
 from pathlib import Path
 from celery.schedules import crontab
 from dotenv import load_dotenv
+from django.core.management.utils import get_random_secret_key
 
 load_dotenv(override=True)
 
@@ -15,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.getenv("DEBUG") == "True" else False
